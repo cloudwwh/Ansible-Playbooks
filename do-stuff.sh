d76e7f35754d8f35e9802d58941161eed1b365f9
cloudwwh <56953724+cloudwwh@users.noreply.github.com>
2020-11-07 05:30:20 +0530

    Update do-stuff.sh

--- do-stuff.sh
+++ do-stuff.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-if [ $1 == "allisnotwell" ]; then             
+#if [ $1 == "alliswell" ]; then
+if [ -z $1 ]
+then
   echo "I CHANGED SOMETHING"
   exit
 else
   echo "I FAILED"
-fi       
+fi
+      
